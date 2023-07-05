c2c64b274a98327473ce8e2cb0fb7ac6dbf70815
Koala <johnpa9191@gmail.com>
2023-07-05 22:33:21 +0800

    upd 7/5

--- canvas.js
+++ canvas.js
@@ -1,18 +1,26 @@
-const canvas = document.getElementById('testscene');
+const canvas = document.getElementById('happyScene');
 const ctx = canvas.getContext('2d');
-const size = 30;
-ctx.fillStyle = 'blue';
+const size = canvas.width/10;
+
+ctx.fillStyle = '#255560';
 
 let x = 0,t=1;
 const id = setInterval(() => {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); 
-  ctx.fillRect(x, 50, size, size);
-  x += t*size;
+    
+    ctx.clearRect(0,0, canvas.width, canvas.height); 
 
-  if (x >= canvas.width) {
-    t=-1;
-  }
-  else if(x<=0){
-    t=1;
-  }
-}, 100);
+
+    ctx.beginPath();
+    ctx.strokeStyle = '#901030';
+    ctx.arc(x+size, 50,size,0,2*Math.PI, false);
+    ctx.stroke();
+    x += t;
+
+    if (x >= canvas.width-2*size) {
+        t=-1;
+    }
+    else if(x<=0){
+        t=1;
+    }
+
+}, 1);
