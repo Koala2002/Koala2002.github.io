54aaebc8296cb8893124bbf94c12007595a6ab86
Koala <johnpa9191@gmail.com>
2023-07-08 15:37:21 +0800

    upd

---
+++ JS/skillTreeMenuBuild.js
@@ -0,0 +1,97 @@
+menu=
+'<div ud="skillMenu" class="skillMenu">\
+    <div id="Sort" class="skillType">\
+        <div class="skillTitle">Sorting Algorithms</div>\
+        <a class="skill" href="SkillTree/BubbleSort.html">Bubble</a>\
+        <a class="skill" href="#">Selection</a>\
+        <a class="skill" href="#">Insertion</a>\
+        <a class="skill" href="#">Merge</a>\
+        <a class="skill" href="#">Quick</a>\
+        <a class="skill" href="#">Heap</a>\
+        <a class="skill" href="#">Radix</a>\
+    </div>\
+    \
+    <div id="Search" class="skillType">\
+        <div class="skillTitle">Searching Algorithms</div>\
+        <a class="skill" href="#">Binary Search</a>\
+        \
+    </div>\
+    <div id="Str" class="skillType">\
+        <div class="skillTitle">String Algorithms</div>\
+        \
+        <a class="skill" href="#">Trie</a>\
+        <a class="skill" href="#">Z</a>\
+        <a class="skill" href="#">KMP</a>\
+        <a class="skill" href="#">Manacher</a>\
+        \
+    </div>\
+    <div id="Math" class="skillType">\
+        <div class="skillTitle">Mathematics</div>\
+        \
+        <a class="skill" href="#">aaaa</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        \
+    </div>\
+    <div id="DP" class="skillType">\
+        <div class="skillTitle">Dynamic Programming</div>\
+        \
+        <a class="skill" href="#">aaaa</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+            \
+    </div>\
+    <div id="OffA" class="skillType">\
+        <div class="skillTitle">Offline Algorithm</div>\
+            \
+        <a class="skill" href="#">aaaa</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        \
+    </div>\
+    <div id="Graph"class="skillType">\
+        <div class="skillTitle">Graph Theory And Tree</div>\
+        \
+        <a class="skill" href="#">aaaa</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        \
+    </div>\
+    <div id="DS" class="skillType">\
+        <div class="skillTitle">Data Structure</div>\
+            \
+        <a class="skill" href="#">aaaa</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">cccc</a>\
+        <a class="skill" href="#">dddd</a>\
+            \
+    </div>\
+    <div id="Empty" class="skillTypeEmpty"></div>\
+    <script src="JS/skillTreeMenu.js"></script>\
+</div>';
+
+document.write(menu);
