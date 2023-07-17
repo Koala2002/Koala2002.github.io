bc728790b42b1c027d85e94c0432b0ba0b1104d1
Koala <johnpa9191@gmail.com>
2023-07-18 05:53:49 +0800

    upd

--- JS/skillTreeMenuBuild.js
+++ JS/skillTreeMenuBuild.js
@@ -2,79 +2,74 @@ menu=
 '<div id="skillMenu" class="skillMenu">\
     <div id="Sort" class="skillType">\
         <div class="skillTitle">Sorting Algorithms</div>\
-        <a class="skill" href="../SkillTree/BubbleSort.html">Bubble</a>\
-        <a class="skill" href="../SkillTree/SelectionSort.html">Selection</a>\
-        <a class="skill" href="../SkillTree/InsertionSort.html">Insertion</a>\
-        <a class="skill" href="../SkillTree/MergeSort.html">Merge</a>\
-        <a class="skill" href="../SkillTree/QuickSort.html">Quick</a>\
-        <a class="skill" href="../SkillTree/HeapSort.html">Heap</a>\
-        <a class="skill" href="../SkillTree/RadixSort.html">Radix</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/BubbleSort.html">Bubble Sort</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/SelectionSort.html">Selection Sort</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/InsertionSort.html">Insertion Sort</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/MergeSort.html">Merge Sort</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/QuickSort.html">Quick Sort</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/HeapSort.html">Heap Sort</a>\
+        <a class="skill" href="../../SkillTree/SortingAlgorithms/RadixSort.html">Radix Sort</a>\
     </div>\
     <div id="Search" class="skillType">\
         <div class="skillTitle">Searching Algorithms</div>\
-        <a class="skill" href="#">Binary Search</a>\
+        <a class="skill" href="../../SkillTree/SearchingAlgorithms/BinarySearch.html">Binary Search</a>\
+        <a class="skill" href="../../SkillTree/SearchingAlgorithms/TwoPointer.html">Two Pointer</a>\
     </div>\
     <div id="Str" class="skillType">\
         <div class="skillTitle">String Algorithms</div>\
-        <a class="skill" href="#">Trie</a>\
-        <a class="skill" href="#">Z</a>\
-        <a class="skill" href="#">KMP</a>\
-        <a class="skill" href="#">Manacher</a>\
+        <a class="skill" href="../../SkillTree/StringAlgorithms/Trie.html">Trie</a>\
+        <a class="skill" href="../../SkillTree/StringAlgorithms/Z.html">Z Algorithm</a>\
+        <a class="skill" href="../../SkillTree/StringAlgorithms/KMP.html">KMP Algorithm</a>\
+        <a class="skill" href="../../SkillTree/StringAlgorithms/Manacher.html">Manacher Algorithm</a>\
     </div>\
     <div id="Math" class="skillType">\
         <div class="skillTitle">Mathematics</div>\
-        <a class="skill" href="#">aaaa</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">Prime Sieve</a>\
+        <a class="skill" href="#">Exponentiation By Squaring</a>\
+        <a class="skill" href="#">Extended Euclidean algorithm</a>\
+        <a class="skill" href="#">Euler&prime;s Function</a>\
+        <a class="skill" href="#">Fermat&prime;s little theorem</a>\
+        <a class="skill" href="#">Modular Multiplicative Inverse</a>\
+        <a class="skill" href="#">Catalan Number</a>\
+        <a class="skill" href="#">Burnside&prime;s Lemma</a>\
+        <a class="skill" href="#">Pólya enumeration theorem</a>\
     </div>\
     <div id="DP" class="skillType">\
         <div class="skillTitle">Dynamic Programming</div>\
-        <a class="skill" href="#">aaaa</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">DP Basic Problems</a>\
+        <a class="skill" href="#">DP Optimization</a>\
     </div>\
     <div id="OffA" class="skillType">\
         <div class="skillTitle">Offline Algorithm</div>\
-        <a class="skill" href="#">aaaa</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">Mo&prime;s Algorithm</a>\
+        <a class="skill" href="#">CDQ Divide and conquer</a>\
     </div>\
     <div id="Graph" class="skillType">\
         <div class="skillTitle">Graph Theory And Tree</div>\
-        <a class="skill" href="#">aaaa</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">bbbb</a>\
+        <a class="skill" href="#">Graph Representation</a>\
+        <a class="skill" href="#">Graph Search</a>\
+        <a class="skill" href="#">Topological Sort</a>\
+        <a class="skill" href="#">Euler Circuit and Path</a>\
+        <a class="skill" href="#">Hamiltonian Circuit and Path</a>\
+        <a class="skill" href="#">Lowest Common Ancestor</a>\
+        <a class="skill" href="#">Euler Tour Of Tree</a>\
+        <a class="skill" href="#">Heavy Light Decomposition</a>\
+        <a class="skill" href="#">DSU on Tree</a>\
+        <a class="skill" href="#">Tarjan Algorithm</a>\
+        <a class="skill" href="#">Minimun Spanning Tree</a>\
+        <a class="skill" href="#">Shortest Path Algorithms</a>\
+        <a class="skill" href="#">Network flow</a>\
     </div>\
     <div id="DS" class="skillType">\
         <div class="skillTitle">Data Structure</div>\
-        <a class="skill" href="#">aaaa</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">dddd</a>\
-        <a class="skill" href="#">bbbb</a>\
-        <a class="skill" href="#">cccc</a>\
-        <a class="skill" href="#">dddd</a>\
+        <a class="skill" href="#">Disjoint Set Union</a>\
+        <a class="skill" href="#">Segment Tree</a>\
+        <a class="skill" href="#">Binary Index Tree</a>\
+        <a class="skill" href="#">Sparse Table</a>\
+        <a class="skill" href="#">Monotonic Stack</a>\
+        <a class="skill" href="#">Monotonic Queue</a>\
     </div>\
-    <script src="../JS/skillTreeMenuSetting.js"></script>\
+    <script src="../../JS/skillTreeMenuSetting.js"></script>\
 </div>\
 <div id="skillMshadow" class="skillMenuShadow"></div>';
 
